7fb5fb3949910cfefc5141376f548d75a6fed0f2
Maciej Kurek <maciejk77@gmail.com>
2015-04-12 21:46:29 +0100

    Before code clean up | Stage 2

--- Gemfile
+++ Gemfile
@@ -5,9 +5,11 @@ ruby '2.2.0'
 gem 'sinatra'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'bcrypt-ruby'
 
 group :test, :development do
   gem 'rspec'
   gem 'database_cleaner'
   gem 'capybara'
 end
+
--- lib/server.rb
+++ lib/server.rb
@@ -1,6 +1,7 @@
 require 'sinatra/base'
 require 'data_mapper'
 require 'tag'
+require 'user'
 
 env = ENV['RACK_ENV'] || 'development'
 
@@ -11,6 +12,9 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
   
   get '/' do
     @links = Link.all
@@ -31,6 +35,21 @@ class BookmarkManager < Sinatra::Base
     tag = Tag.first(text: params[:text])
     @links = tag ? tag.links : []
     erb :index
+  end
+
+  get '/users/new' do
+  # note the view is in views/users/new.erb
+  # we need the quotes because otherwise
+  # ruby would divide the symbol :users by the
+  # variable new (which makes no sense)
+    erb :'users/new'
+  end
+
+  post '/users' do
+  User.create(email: params[:email],
+              password: params[:password])
+  session[:user_id] = user.id
+  redirect to('/')
   end
 
   # start the server if ruby file executed directly
