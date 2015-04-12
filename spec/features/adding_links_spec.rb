11d0c0fc19777e091da5e3c629111c987250bd3d
Maciej Kurek <maciejk77@gmail.com>
2015-04-12 16:59:15 +0100

    Completed | Stage 3

--- spec/features/adding_links_spec.rb
+++ spec/features/adding_links_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 feature 'User adds a new link' do
   scenario 'when browsing the homepage' do
     expect(Link.count).to eq(0)
@@ -11,23 +9,22 @@ feature 'User adds a new link' do
     expect(link.title).to eq('Makers Academy')
   end
 
-  scenario 'with a few tags' do
-    visit '/'
-    add_link('http://www.makersacademy.com', 
-             'Makers Academy',
-             ['education', 'ruby'])
-    link = Link.first
-    expect(link.tags.map(&:text).to include('education'))
-    expect(link.tags.map(&:text).to include('ruby'))
-  end
-
   def add_link(url, title, tags=[])
     within('#new-link') do
       fill_in 'url', with: url
       fill_in 'title', with: title
-      fill_in 'tags', with: tags.join('')
+      fill_in 'tags', with: tags.join(' ')
       click_button 'Add link'
     end
   end
 
+
+  scenario 'with a few tags' do
+    visit '/'
+    add_link('http://www.makersacademy.com', 
+             'Makers Academy',
+             %w(education ruby))
+    link = Link.first
+    expect(link.tags.map(&:text)).to include('education', 'ruby')
+  end
 end
--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -4,12 +4,31 @@ feature 'User browses the list of links' do
 
   before(:each) do
     Link.create(url: 'http://www.makersacademy.com',
-                title: 'Makers Academy')
+                title: 'Makers Academy', 
+                tags: [Tag.first_or_create(text: 'education')])
+    Link.create(url: 'http://ww.google.com', 
+                title: 'Google',
+                tags: [Tag.first_or_create(text: 'search')])
+    Link.create(url: 'http://ww.google.com', 
+                title: 'Bing',
+                tags: [Tag.first_or_create(text: 'search')])
+     Link.create(url: 'http://ww.google.com', 
+                title: 'Code.org',
+                tags: [Tag.first_or_create(text: 'education')])
   end
   
   scenario 'when opening the home page' do
     visit '/'    
     expect(page).to have_content('Welcome')
   end
+
+  scenario 'filtered by a tag' do
+    visit 'tags/search'
+    expect(page).not_to have_content('Makers Academy')
+    expect(page).not_to have_content('Code.org')
+    expect(page).to have_content('Google')
+    expect(page).to have_content('Bing')
+  end
+
 
 end
